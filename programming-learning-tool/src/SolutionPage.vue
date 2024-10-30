393bfdf9d9041911c3c30ca66edb0dd015aeacea
ZumzarHS <rasmus.play@gmail.com>
2024-10-30 09:08:16 +0100

    a bit of css cleanup

--- programming-learning-tool/src/SolutionPage.vue
+++ programming-learning-tool/src/SolutionPage.vue
@@ -77,20 +77,20 @@ const submitProgLanguage = async (language: string): Promise<void> => {
         Programming Language:
         <Menu as="div" class="relative inline-block text-left">
           <div>
-            <MenuButton class="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+            <MenuButton class="dropdown-menu-button">
               {{ selectedProgLanguage }}
-              <ChevronDownIcon class="-mr-1 h-5 w-5 text-gray-400" aria-hidden="true" />
+              <ChevronDownIcon class="dropdown-arrow" aria-hidden="true" />
             </MenuButton>
           </div>
 
           <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
-            <MenuItems class="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+            <MenuItems class="dropdown-menu-items">
               <div class="py-1">
                 <MenuItem v-slot="{ active }">
-                  <a href="#" @click="() => { submitProgLanguage('Python'); }" :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block px-4 py-2 text-sm']">Python</a>
+                  <a href="#" @click="() => { submitProgLanguage('Python'); }" :class="[active ? 'hover-dropdown-item' : 'non-hover-dropdown-item', 'dropdown-menu-item ']">Python</a>
                 </MenuItem>
                 <MenuItem v-slot="{ active }">
-                  <a href="#" @click="() => { submitProgLanguage('C'); }" :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block px-4 py-2 text-sm']">C</a>
+                  <a href="#" @click="() => { submitProgLanguage('C'); }" :class="[active ? 'hover-dropdown-item' : 'non-hover-dropdown-item', 'dropdown-menu-item ']">C</a>
                 </MenuItem>
               </div>
             </MenuItems>
@@ -99,12 +99,12 @@ const submitProgLanguage = async (language: string): Promise<void> => {
       </div>
       <!-- Right Top Container -->
       <div class="container-buttons">
-        <button type="button" class="button" @click="submitCode">
+        <button type="button" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click="submitCode">
           Submit
         </button>
-        <div class="button">
+        <button type="button" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
           Run
-        </div>
+        </button>
       </div>
     </div>
     <div class="top-containers-wrapper">
