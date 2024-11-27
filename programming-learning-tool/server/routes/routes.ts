efb70a458e00aed4cd7b127b4616f2ba1d8a5068
Mathias Pallesen Munch <mpmunch@gmail.com>
2024-11-27 11:57:17 +0100

    updated compiler options to remove warning if no default import

--- programming-learning-tool/server/routes/routes.ts
+++ programming-learning-tool/server/routes/routes.ts
@@ -1,4 +1,4 @@
-import * as express from 'express';
+import express from 'express';
 import cors from 'cors';
 import * as compilercontroller from '../controllers/compiler';
 import { getTasks, getTaskById } from '../controllers/tasks';
