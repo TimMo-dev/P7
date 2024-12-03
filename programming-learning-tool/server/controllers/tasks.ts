a627310e1ffc65a33d050c3160b2c2927db2f788
Tim Morakran <timyaluan@gmail.com>
2024-12-03 13:08:46 +0100

    changed taskid to id

--- programming-learning-tool/server/controllers/tasks.ts
+++ programming-learning-tool/server/controllers/tasks.ts
@@ -24,7 +24,7 @@ export async function getTaskById(req, res) {
   const { id } = req.params;
   try {
     console.log(`Fetching task with ID ${id} from the database...`);
-    const result = await pool.query('SELECT * FROM Programming_task WHERE taskid = $1', [id]);
+    const result = await pool.query('SELECT * FROM Programming_task WHERE id = $1', [id]);
     if (result.rows.length > 0) {
       console.log('Task fetched successfully:', result.rows[0]);
       res.json(result.rows[0]);
